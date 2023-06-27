e833614727de6c507dd0759c397404171639e587
Ricardo Ribeiro <rlribeiro@hotmail.com.br>
2023-06-27 15:21:48 -0300

    Initial Commit

---
+++ JobsitySeriesApp/JobsitySeriesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobsitySeriesAppApp.swift
+//  JobsitySeriesApp
+//
+//  Created by Ricardo Ribeiro on 27/06/23.
+//
+
+import SwiftUI
+
+@main
+struct JobsitySeriesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
